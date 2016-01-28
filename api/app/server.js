f73ee0ee8623646d3f5bff7159a779549d7f74ae
xiaodun0571 <xiaodun0571@163.com>
2016-01-28 19:26:47 +0800

    前台api

--- api/app/server.js
+++ api/app/server.js
@@ -2,31 +2,17 @@
  * socket 服务端
  * Created by mouse on 16/1/23.
  */
-<<<<<<< HEAD
-/**
- * 服务端
- */
-=======
->>>>>>> origin/master
 var http = require('http'),
     sio = require('socket.io'),
 
 //自定义模块
     config = require('./config/config'),
-<<<<<<< HEAD
-    comm = 	require('./lib/socket');
-=======
     comm = 	require('./libs/socket');
->>>>>>> origin/master
 var server = http.createServer();
 var io = sio.listen(server);
 var usersWS = {};
 io.sockets.on('connection', function(socket){
-<<<<<<< HEAD
     console.log("来自客户端 " + socket.id + " ip="+socket.handshake.address+" 连接.");
-=======
-    console.log("from client :" + socket.id + " ip="+socket.handshake.address+" content.");
->>>>>>> origin/master
     var test = function() {
         for (var i in usersWS){
             // 同一个ip只有一个连接
@@ -53,11 +39,7 @@ io.sockets.on('connection', function(socket){
     //客户的断开连接
     socket.on('disconnect', function(){
         delete usersWS[socket.handshake.address];
-<<<<<<< HEAD
         console.log("客户端 " + socket.id + " 断开连接.");
-=======
-        console.log("client " + socket.id + " close.");
->>>>>>> origin/master
         refresh_online();
     });
     //获得客户端emit数据
